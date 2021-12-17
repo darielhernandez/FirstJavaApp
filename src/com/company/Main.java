7d78f861b91939f5cfeef662aabe48ed79526ef0
darielhernandez <13dariel@gmail.com>
2021-12-16 21:35:14 -0500

    Implement getFirstElement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println(getFirstElement(new int[]{3,1,2}));
+    }
+    public static int getFirstElement(int[] arr){
+        return arr[0];
     }
 }
