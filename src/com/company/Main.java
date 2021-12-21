d4babc41f69c3a806e1e94ebb2b03f35265f640b
darielhernandez <13dariel@gmail.com>
2021-12-20 19:27:59 -0500

    implement alg. 2 & 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,49 @@
 package com.company;
 
+import java.util.Enumeration;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println(getFirstElement(new int[]{3,1,2}));
+        System.out.println(getFirstValue(new int[]{3,1,7}));
     }
-    public static int getFirstElement(int[] arr){
+//algorithm 1 getFirstElement
+    public static int getFirstValue(int[] arr) {
         return arr[0];
     }
+    //algorithm 2 nextNum
+
+    public static int addition(int num) {
+        return ++num;
+    }
+
+    //algorithm 3- findTheIndex
+    public static int search(int[] arr, int item) {
+        int index=-1;
+        for(int i=0; i < arr.length;i++){
+            if(arr[i]==item){
+                index = i;
+                i+=arr.length+1;
+            }
+        }
+        return index;
+    }
+
+
+
+    //algorithm 4- minToSec
+
+
+
+
+
+    //algorithm 5- returnSomething
+
+
+
+
+
+
+
 }
